fa654fc758acc0260bfcc679f2cf8c9e2d57d49b
1ibrahimtsc <1ibrahimtsc@gmail.com>
2023-06-15 08:39:01 +0600

    some trying

--- summer-camp-server-deployment/index.js
+++ summer-camp-server-deployment/index.js
@@ -479,7 +479,16 @@ async function run() {
       const query = {
         _id: { $in: payment.cartItems.map((id) => new ObjectId(id)) },
       };
+
+
       const deleteResult = await cartCollection.deleteMany(query);
+
+      
+
+      const result = await classesCollection.updateOne(
+        query,
+        updateDoc
+      );
 
       res.send({ insertResult, deleteResult });
     });
